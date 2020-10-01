3cb864e8c156e44f7c9ac096c2c136f5ffd22fed
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-10-01 13:06:54 -0500

    added dots

--- js/main.js
+++ js/main.js
@@ -40,3 +40,51 @@ d3.csv("https://raw.githubusercontent.com/jbraunschweiger/CSE457-Studio3/master/
     console.error("Error loading data.");
 });
 
+d3.csv("https://raw.githubusercontent.com/jbraunschweiger/CSE457-Studio3/master/data/cities.csv?token=AF7MUKWPCPQB4MYZ47E7POK7P5GNM").then(data => {
+    var filteredData = data.filter(function(d){
+        return d.eu === "true";
+    });
+    d3.select("div").append("h2").text(filteredData.length + " Cities");
+    var citySVG = d3.select("div").append("svg").attr("width",700).attr("height", 550);
+
+    citySVG.selectAll("cirle")
+        .data(filteredData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d){
+            return d.x;
+        })
+        .attr("cy", function(d){
+            return d.y;
+        })
+        .attr("r", function(d) {
+            if(d.population < 1000000) {
+                return 4;
+            } else {
+                return 8;
+            }
+        })
+        .attr("class", "city-dot");
+
+    citySVG.selectAll("text")
+        .data(filteredData)
+        .enter()
+        .append("text")
+        .attr("x", function(d){
+            return d.x;
+        })
+        .attr("y", function(d){
+            return d.y - 12;
+        })
+        .text(function(d) {
+            return d.city;
+        })
+        .attr("opacity", function(d){
+            if(d.population < 1000000) {
+                return 0;
+            } else {
+                return 1;
+            }
+        })
+        .attr("class", "city-label");
+});
